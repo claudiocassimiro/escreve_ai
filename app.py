73cf7e62a74200094ebc5823fb0eac8fd73bae7d
claudiocassimiro <days_surfing@live.com>
2024-03-15 20:20:50 -0300

    feat: implements app Flask and /sms router

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+from constumer_support_crew import ConstumerSupportCrew
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    body = request.values.get('Body', None)
+    crew = ConstumerSupportCrew(body)
+    response = crew.run()
+
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message(response)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
