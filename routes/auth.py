a2c89b07463c4313f402b6ed99d9e5fc1ed9fb36
claudiocassimiro <days_surfing@live.com>
2024-03-19 16:51:02 -0300

    feat: implements auth router with endpoints to create user and login user

---
+++ routes/auth.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import create_access_token, create_refresh_token
+from models import User
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/api/register', methods=['POST'])
+def register_user():
+  data = request.get_json()
+
+  user = User.get_user_by_username(username = data.get('username'))
+
+  if user is not None:
+    return jsonify({"error": "User already exists"}), 409
+  
+  new_user = User(
+    username = data.get('username'),
+    email = data.get('email'),
+  )
+
+  new_user.set_password(password = data.get('password'))
+
+  new_user.save()
+  
+  return jsonify({"message": "User created"}), 201
+
+@auth_bp.post('/api/login')
+def login_user():
+  data = request.get_json()
+
+  user = User.get_user_by_username(username = data.get('username'))
+
+  if user and (user.check_password(password = data.get('password'))):
+    access_token = create_access_token(identity=user.username)
+    refresh_token = create_refresh_token(identity=user.username)
+    
+    return jsonify({
+      "message": "Logged In", 
+      "token": {
+        "access": access_token,
+        "refresh": refresh_token 
+      }
+    }), 200
+  
+  return jsonify({"error": "Invalid password or password"}), 400
+  
+
+
