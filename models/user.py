78f2da909c2fd5aac1ed533d3a4ff529665a36b4
claudiocassimiro <days_surfing@live.com>
2024-03-22 20:08:47 -0300

    feat: add relation between user table and template and post tables

--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
 # models.py
 from app import db
-from uuid import uuid4
+from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
+from .template import Template
+from .post import Post
 
 class User(db.Model):
     __tablename__ = "users"
@@ -10,7 +12,9 @@ class User(db.Model):
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
-    
+    Templates = relationship(Template, backref="users")
+    Posts = relationship(Post, backref="users")
+
     def json(self):
         return { "id": self.id, "username": self.username, "email": self.email, "password": self.password }
 
