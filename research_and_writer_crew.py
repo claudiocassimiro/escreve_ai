cef44f9b8c40275b88f10f3c2fdc110d36198b92
claudiocassimiro <days_surfing@live.com>
2024-03-22 21:55:19 -0300

    feat: implements logic to take user writing format

--- research_and_writer_crew.py
+++ research_and_writer_crew.py
@@ -4,13 +4,14 @@ from research_and_writer_agents import ResearchAndWriterAgents
 from research_and_writer_tasks import ResearchAndWriterTasks
 
 class ResearchAndWriterCrew:
-    def __init__(self, theme_to_search):
+    def __init__(self, user_writing_format, theme_to_search):
+        self.user_writing_format = user_writing_format
         self.theme_to_search = theme_to_search
 
     def run(self):
         # Define your custom agents and tasks in agents.py and tasks.py
         research_and_write_team = ResearchAndWriterAgents()
-        research_and_write_tasks = ResearchAndWriterTasks(self.theme_to_search)
+        research_and_write_tasks = ResearchAndWriterTasks(self.user_writing_format, self.theme_to_search)
 
         # Define your custom agents and tasks here
         research = research_and_write_team.research()
