788bba5e2a4205a351b71f17d17f4303d49ae8e9
claudiocassimiro <days_surfing@live.com>
2024-03-22 21:55:01 -0300

    feat: add new blueprints routes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from os import environ
 from flask import Flask
 from utils.extensions import db, jwt
-from routes.auth import auth_bp
+from routes import auth, template, post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
@@ -9,7 +9,9 @@ app.config['JWT_SECRET_KEY'] = environ.get('JWT_SECRET_KEY')
 db.init_app(app)
 jwt.init_app(app)
 
-app.register_blueprint(auth_bp)
+app.register_blueprint(auth.auth_bp)
+app.register_blueprint(template.template_bp)
+app.register_blueprint(post.post_bp)
 
 with app.app_context():
     db.create_all()
